b3234f9272e89764c5f8cc7347e110f6f7f90737
Mayur Rathod <85155924+mayur-r-d@users.noreply.github.com>
2022-08-23 16:23:38 +0530

    - Generic FSM added

---
+++ Assets/Src/StateMachine/GenericFSM.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+
+//Reference :  https://faramira.com/generic-finite-state-machine-using-csharp/
+namespace Patterns
+{
+    public class GenericFSM<T>
+    {
+        protected Dictionary<T, State<T>> mStates;
+
+        protected State<T> mPreviousState;
+
+        protected State<T> mCurrentState;
+
+        public GenericFSM()
+        {
+            mStates = new Dictionary<T, State<T>>();
+        }
+
+        public void Add(State<T> state)
+        {
+            mStates.Add(state.ID, state);
+        }
+
+        public void Add(T stateID, State<T> state)
+        {
+            mStates.Add(stateID, state);
+        }
+
+        public State<T> GetState(T stateID)
+        {
+            if(mStates.ContainsKey(stateID))
+                return mStates[stateID];
+            return null;
+        }
+
+        public void SetCurrentState(T stateID)
+        {
+            State<T> state = mStates[stateID];
+            SetCurrentState(state);
+        }
+
+        public State<T> GetPreviousState()
+        {
+            return mPreviousState;
+        }
+
+        public State<T> GetCurrentState()
+        {
+            return mCurrentState;
+        }
+
+        public void SetCurrentState(State<T> state)
+        {
+            if (mCurrentState == state)
+            {
+                return;
+            }
+
+            if (mCurrentState != null)
+            {
+                mCurrentState.Exit();
+            }
+
+            mPreviousState = mCurrentState != null ? mCurrentState : state;
+            mCurrentState = state;
+
+            if (mCurrentState != null)
+            {
+                mCurrentState.Enter();
+            }
+        }
+
+        public void Update(float dt)
+        {
+            if (mCurrentState != null)
+            {
+                mCurrentState.Update(dt);
+            }
+        }
+    }
+}
---
+++ Assets/Src/StateMachine/State.cs
@@ -0,0 +1,27 @@
+﻿namespace Patterns
+{
+    public abstract class State<T>
+    {
+        public string Name { get; set; }
+
+        public T ID { get; private set; }
+
+        public State(T id)
+        {
+            ID = id;
+        }
+        public State(T id, string name) : this(id)
+        {
+            Name = name;
+        }
+
+        public abstract void Enter();
+        public abstract void Exit();
+        /// <summary>
+        /// Updates States
+        /// </summary>
+        /// <param name="dt">delta time</param>
+        public abstract void Update(float dt);
+    }
+}
+
